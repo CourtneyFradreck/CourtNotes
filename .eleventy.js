44ad8d35f81ddbf037300539b80c1e01af5b2bdb
Courtney Fradreck <courtaga@gmail.com>
2025-02-08 10:39:49 +0200

    update Eleventy configuration to remove unnecessary passthrough copies and add includes directory

--- .eleventy.js
+++ .eleventy.js
@@ -4,14 +4,13 @@ module.exports = function(eleventyConfig) {
     eleventyConfig.addPassthroughCopy("src/assets");
     eleventyConfig.addPassthroughCopy("src/style.css");
     eleventyConfig.addPassthroughCopy("src/individual.css");
-    eleventyConfig.addPassthroughCopy("src/all-blogs.html");
-    eleventyConfig.addPassthroughCopy("src/individual-blog.html");
     eleventyConfig.addFilter("postDate", (dateObj) => {
         return DateTime.fromJSDate(dateObj).toLocaleString(DateTime.DATE_MED);
     });
     return {
         dir: {
             input: "src",
+            includes: "_includes",
             output: "public"
         }
     };

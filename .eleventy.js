18220d338ac47099abafac4b3624cd9067de5465
Courtney Fradreck <courtaga@gmail.com>
2025-11-27 15:11:11 +0200

    feat: Implement comprehensive SEO features including robots.txt, meta tags, and a new date filter for Eleventy.

--- .eleventy.js
+++ .eleventy.js
@@ -8,16 +8,23 @@ const siteMetadata = {
     siteUrl: "https://courtnotes.netlify.app"
 };
 
-module.exports = function(eleventyConfig) {
+module.exports = function (eleventyConfig) {
     eleventyConfig.addPassthroughCopy("src/assets");
     eleventyConfig.addPassthroughCopy("src/style.css");
     eleventyConfig.addPassthroughCopy("src/individual.css");
     eleventyConfig.addPassthroughCopy("src/admin");
     eleventyConfig.addPassthroughCopy("src/admin/config.yml");
-    eleventyConfig.addPassthroughCopy({"_headers": "_headers"});
+    eleventyConfig.addPassthroughCopy({ "_headers": "_headers" });
+
     eleventyConfig.addFilter("postDate", (dateObj) => {
         return DateTime.fromJSDate(dateObj).toLocaleString(DateTime.DATE_MED);
     });
+
+    // Date filter for sitemap
+    eleventyConfig.addFilter("date", (dateObj, format) => {
+        return DateTime.fromJSDate(dateObj).toFormat(format);
+    });
+
     eleventyConfig.addCollection("featured", function (collectionApi) {
         return collectionApi.getFilteredByTag("featured");
     });
